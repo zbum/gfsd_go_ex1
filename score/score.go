57f76ce845c13ac5d9cd4317e26da15e26da7ce1
Manty <jibum.jung@nhndooray.com>
2024-03-18 09:29:43 +0900

    add score function

--- score/score.go
+++ score/score.go
@@ -9,14 +9,14 @@ type Score struct {
 
 type ScoreResponse struct {
 	Id        int64 `json:"id"`
-	Semester  int
-	StudentId int64
-	Score     int
+	Semester  int   `json:"semester"`
+	StudentId int64 `json:"studentId"`
+	Score     int   `json:"score"`
 }
 
 type ScoreRequest struct {
-	Id        int64
-	Semester  int
-	StudentId int64
-	Score     int
+	Id        int64 `json:"id"`
+	Semester  int   `json:"semester"`
+	StudentId int64 `json:"studentId"`
+	Score     int   `json:"score"`
 }
